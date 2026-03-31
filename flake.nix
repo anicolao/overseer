01eabdfc930dd0bc7c7bf607e833e6a5f1fd6182
Alex Nicolaou <anicolao@gmail.com>
2026-03-31 15:37:22 -0400

    feat: implement Nix-based VM architecture and wire-test

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description: "Overseer - Autonomous Agent Coordination System";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            typescript
+            gh
+            git
+          ];
+
+          shellHook = ''
+            echo "Overseer Development Environment Initialized"
+            node --version
+            gh --version
+          '';
+        };
+      }
+    );
+}
