e8a2c7e6b5c1185630d1c92ff4e373de07f0c882
anicolao <anicolao@gmail.com>
2026-03-31 20:17:16 -0400

    feat: implementation for #25

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+      rules: {
+          "@typescript-eslint/no-explicit-any": "off"
+      }
+  }
+];
